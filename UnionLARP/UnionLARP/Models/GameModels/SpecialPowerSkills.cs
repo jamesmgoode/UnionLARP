eea73f1396bfaa686321a6eb087c04a3cb134d9c
James Goode <james_goode@hotmail.co.uk>
2018-12-30 12:16:49 +0000

    Master sln for all sites on union larp

--- UnionLARP/UnionLARP/Models/GameModels/SpecialPowerSkills.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UnionLARP.Models.GameModels
-{
-    public class SpecialPowerSkills
-    {
-        public int Id { get; set; }
-        public int SpecialPowerId { get; set; }
-        public int SkillId { get; set; }
-    }
-}
